071105e34fe499abfa9b2232f00c280a550a2485
Ujjal Dey Sarkar <ujjaldeysarkar123@gmail.com>
2025-03-04 10:13:48 +0000

    send welcome mail to user after registration

--- project_management/settings.py
+++ project_management/settings.py
@@ -193,3 +193,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+# Email Backend Configuration
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"  # Use your email provider's SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an app password for security
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
