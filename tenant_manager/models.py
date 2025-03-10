7ac4c7683268d0fc010ed5ddd9ffdfca8ceea74a
Sourav Mandal <myselfsourav20@gmail.com>
2025-03-10 07:32:09 +0000

    removed django codebase

--- tenant_manager/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django_tenants.models import TenantMixin, DomainMixin
-
-class Tenant(TenantMixin):
-    name = models.CharField(max_length=255)
-    created_on = models.DateField(auto_now_add=True)
-    
-class Domain(DomainMixin):
-    pass
