89350bf11f64f132580f7ab962a1181e0e646075
pioneerTesting <pioneertesting2025@gmail.com>
2025-03-12 18:59:57 +0530

    init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+import { UserMenu } from "./UserMenu";
+
+interface HeaderProps {
+  className?: string;
+}
+// className="h-f flex justify-center items-center"
+
+export const Header = ({ className }: HeaderProps) => {
+  return (
+    <header
+      className={cn(
+        "fixed items-center top-0 w-full z-50 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60",
+        className
+      )}
+    >
+      <div className="container flex h-16 items-center justify-between">
+        <Link
+          href={"/"}
+          className="flex items-center space-x-2 font-bold text-xl hover:text-primary transition-colors"
+        >
+          UnManage AI
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <UserMenu />
+          <div className="border-l pl-4 dark:border-gray-800">
+            <ThemeToggle />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
