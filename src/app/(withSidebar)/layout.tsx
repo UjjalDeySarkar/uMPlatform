cd06c00c33d919edff90605d454b3f8e1775dfed
Sourav Mandal <myselfsourav20@gmail.com>
2025-03-17 09:29:12 +0000

    updating dashboard

---
+++ src/app/(withSidebar)/layout.tsx
@@ -0,0 +1,33 @@
+import { AppSidebar } from "@/components/AppSidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { createClient } from "@/../utils/supabase/server";
+
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const supabase = await createClient();
+  const { data } = await supabase.auth.getUser();
+
+  const user = data?.user?.user_metadata
+
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar user={user} className="pt-10"/>
+        <SidebarTrigger className="-ml-1" />
+        <main className="flex-1 p-2">{children}</main>
+      </SidebarProvider>
+    </>
+  )
+}
